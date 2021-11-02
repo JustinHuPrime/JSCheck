eaf02dcb390300df3c0e5dea7d6d8dbbd516ddfb
Justin Hu <justin.hu@alumni.ubc.ca>
2021-11-01 17:15:10 -0700

    vague outline

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,5 @@
+// command line specifies list of files
+// read all files in via babel
+// traverse the AST and save error reports to a global structure as you go
+// output errors
 console.log("Hello, world!");
