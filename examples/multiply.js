104bd410ecc8572d9d69dd5e22e935048edec1d0
curtiskoo <36326572+curtiskoo@users.noreply.github.com>
2021-11-10 04:42:16 -0800

    adding some error message levels for user study 1 (#19)

--- examples/multiply.js
+++ examples/multiply.js
@@ -5,12 +5,17 @@ function dbl(number) {
 if (require.main === module) {
   let nums = [12, 23, 40, undefined, 64, undefined, 881];
 
-  let res = []
+  let res = [];
 
   nums.forEach((num, idx) => { res[idx] = dbl(num) });
 }
 
 /**
- * Warning: Multiplication on non-numerical type on line 2
+ * (Level 1 Default JS) <No error>
+ *
+ * (Level 2) Warning: Multiplication on non-numerical type on line 2
+ *
+ * (Level 3) Warning: Multiplication on non-numerical type on line 2
  * Attempted to call '*' on type undefined, expected number (2:3)
+ *
  */
