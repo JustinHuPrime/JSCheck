1e2509dd96b5c245cf2039d249df84dec3fe039d
Wong Kean Wah <2560596+kilowatt-@users.noreply.github.com>
2021-11-22 18:04:46 -0800

    Updated cursed examples

--- examples/fizzybuzzy.js
+++ examples/fizzybuzzy.js
@@ -8,7 +8,8 @@
  * In this modified version, we take in an arbitrary list of numbers as input
  * and return a list of string / number results instead of printing
  */
-function fizzBuzzList(inputs) {
+const inputs = [...Array(20).keys()];
+
     let outputs = [];
     if (inputs % 15 == 0) {
         outputs.push("Fizzbuzz");
@@ -21,38 +22,17 @@ function fizzBuzzList(inputs) {
     } else {
         outputs.push(inputs);
     }
-    return outputs;
-}
 
-/**
- * Count the amount of times "buzz" appears in the fizzBuzzList result for inputs,
- * including as either part of "Buzz" or "Fizzbuzz"
- */
-function countBuzz(inputs) {
-    let lst = fizzBuzzList(inputs);
     let result = 0;
     for (let value of lst) {
         if (value.match(/buzz/i)) {
             result += 1;
         }
     }
-    return result;
-}
-
-if (require.main === module) {
-    console.log(countBuzz([...Array(20).keys()]));
-}
 
 /**
  * (Level 1 Default JS) TypeError: value.match is not a function (35:19)
  *
- * (Level 2) TypeError: Attempted to call 'match' on type object (35:19)
- *
- * (Level 3) TypeError: Attempted to call 'match' on a list,
- * did you mean to call 'match' on a string instead? (35:19)
- *
- * (Level 4) TypeError: Attempted to call 'match' on a list,
- * did you mean to call 'match' on a string instead? (35:19)
- * Warning: 'fizzBuzzList' returns conflicting types: string[] or any[]
+ * (Level 2) TypeError: Attempted to call 'match' on type Array[Array[Number]] (35:19)
  *
  * **/
