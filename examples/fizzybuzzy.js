9f1d6e847ff5b411c6240cf42f938294cdf05e51
Wong Kean Wah <2560596+kilowatt-@users.noreply.github.com>
2021-11-22 20:09:52 -0800

    changed inputs

--- examples/fizzybuzzy.js
+++ examples/fizzybuzzy.js
@@ -8,7 +8,7 @@
  * In this modified version, we take in an arbitrary list of numbers as input
  * and return a list of string / number results instead of printing
  */
-const inputs = [...Array(20).keys()];
+const inputs = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 
 let outputs = [];
 if (inputs % 15 == 0) {
